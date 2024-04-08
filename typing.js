b372cd68be3507db2bff1feb31a47fc4e09d4c8d
ASH <singhashirwad2003@gmail.com>
2024-04-08 19:01:47 +0530

    first commit

---
+++ typing.js
@@ -0,0 +1,9 @@
+// Splitting the string at spaces
+const words = 'http:/ / 127.0.0.1:5500 /index. html'.split(' ');
+
+// Defining a function to clear the inner HTML of an element with id 'words'
+function newGame() {
+   document.getElementById('words').innerHTML = '';
+}
+
+
